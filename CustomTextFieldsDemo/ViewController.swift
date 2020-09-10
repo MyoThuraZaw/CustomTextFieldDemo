30cc1d52c48caf9134ab4387066f01e1a44425c0
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2020-09-10 18:36:35 +0630

    Initial Commit

---
+++ CustomTextFieldsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTextFieldsDemo
+//
+//  Created by Myo Thura Zaw on 9/10/20.
+//  Copyright © 2020 Myo Thura Zaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
